2d3b93c9b1681b812d15de73d57c0db81becaf96
Sasha Sorokin <dafri.nochiterov8@gmail.com>
2019-11-22 10:44:52 +0700

    refactor: документация для известных методов repo

--- src/yandex/repo.ts
+++ src/yandex/repo.ts
@@ -12,20 +12,46 @@ declare global {
 		CRACKDOWN_PAUSE: string;
 	}
 
+	/**
+	 * Представляет собой класс плеера
+	 */
 	interface IYandexMusicPlayer {
+		/**
+		 * Загрузился ли плеер
+		 */
 		isReady(): boolean;
 	}
 
+	/**
+	 * Представляет собой хэш-карту, репозиторий загруженных классов
+	 */
 	interface IYandexMusicDiRepo {
+		/**
+		 * Инстанс плеерам
+		 */
 		player: IYandexMusicPlayer;
+		/**
+		 * Адаптер уведомлений
+		 */
 		adapter: IYandexMusicAdapter;
 	}
 
 	interface IYandexMusicDi {
+		/**
+		 * Хэш-карта, репозиторий загруженных классов
+		 */
 		repo: IYandexMusicDiRepo;
 	}
 
+	/**
+	 * Представляет собой класс интерактивных уведомлений
+	 */
 	interface IYandexMusicNotify {
+		/**
+		 * Отображает новое уведомление над плеером на короткое время
+		 *
+		 * @param text Текст для отображения в уведомлении
+		 */
 		show(text: string): void;
 	}
 
@@ -34,40 +60,96 @@ declare global {
 		domDelegate(): void;
 	}
 
+	/**
+	 * Опции для создания нового элемента
+	 */
 	interface IYandexBlockOptions<K extends string> {
+		/**
+		 * Тип элемента
+		 */
 		type: K;
+		/**
+		 * Дополнительные данные, необходимые для создания элемента
+		 */
 		data: unknown;
 	}
 
+	/**
+	 * Представляет собой субтип всех элементов, которые возможно сконструировать,
+	 * используя метод `IYandexMusicBlocks#createBlock`
+	 */
 	type YandexMusicBlockCostructors = SubType<IYandexMusicBlocks["blocks"], () => IYandexMusicBlock>;
 
+	/**
+	 * Представляет собой коллекцию интерактивных элементов
+	 */
 	interface IYandexMusicBlocks {
 		di: IYandexMusicDi;
+		/**
+		 * Хэш-карта всех возможных элементов
+		 */
 		blocks: {
+			/**
+			 * Класс уведомлений над проигрывателем
+			 */
 			notify: IYandexMusicNotify;
+			/**
+			 * Класс селектора
+			 */
 			"d-select"(): IYandexMusicBlock;
 		};
 
+		/**
+		 * Возвращает элемент под указанным названием
+		 *
+		 * @param name Название элемента
+		 * @returns Элемент под указанным названием
+		 */
 		get<K extends keyof IYandexMusicBlocks["blocks"]>(name: K): IYandexMusicBlocks["blocks"][K];
 
+		/**
+		 * Создаёт новый интерактивный элемент
+		 *
+		 * @param creationOptions Опции для создания нового интерактивного элемента
+		 * @param container Контейнер элементов для этого интерактивного элемента
+		 * @returns Созданный интерактивный элемент
+		 */
 		createBlock<K extends keyof YandexMusicBlockCostructors>(
 			creationOptions: IYandexBlockOptions<K>,
 			container: HTMLElement,
 		): ReturnType<YandexMusicBlockCostructors[K]>;
 	}
 
+	/**
+	 * Представляет собой настройки пользователя
+	 */
 	interface IYandexSettings {
+		/**
+		 * Установленный пользователем язык
+		 */
 		lang: string;
 	}
 
+	/**
+	 * Представляет собой основное внутреннее API Яндекс.Музыки
+	 */
 	interface IYandexMusicAPI {
 		Adapter: IYandexMusicAdapterStatic;
+		/**
+		 * Коллекция интерактивных элементов
+		 */
 		blocks: IYandexMusicBlocks;
+		/**
+		 * Настройки пользователя
+		 */
 		settings: IYandexSettings;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/interface-name-prefix
 	interface Window {
+		/**
+		 * Основное внутреннее API Яндекс.Музыки
+		 */
 		Mu: IYandexMusicAPI;
 	}
 }
